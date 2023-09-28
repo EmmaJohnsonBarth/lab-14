8774370b71b23edcc0496e298a8da4367d93b896
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-09-28 12:33:12 -0500

    added to funcs in app.js

--- js/app.js
+++ js/app.js
@@ -7,7 +7,8 @@ function AppState() {
 AppState.prototype.instantiateProducts = function () {
 
   const productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'water-can', 'wine-glass'];
-
+  console.log('productNames array: ', productNames);
+  
   for (let i = 0; i < productNames.length; i++) {
     if (productNames[i] === 'sweep') {
       this.allProducts.push(new Product(productNames[i], 'png'))
@@ -20,15 +21,25 @@ AppState.prototype.instantiateProducts = function () {
 
 AppState.prototype.saveToLocalStorage = function () {
   // TODO: Fill in this instance method to save product data to local storage
+  // event.preventDefault();
+  let newProduct = JSON.stringify(this.allProducts)
+  console.log('newProductStringified: ', newProduct)
+  localStorage.setItem('key', newProduct);
+
 }
+
+
+
 
 AppState.prototype.loadItems = function () {
-
   // TODO: Update this instance method to retrieve data from local storage instead of creating new Products on each page load
+  localStorage.getItem(this.allProducts);
 
+  //provided, don't delete:
   this.instantiateProducts();
-
 }
+
+
 
 
 function Product(name, fileExtension = 'jpg') {
--- js/voting.js
+++ js/voting.js
@@ -11,28 +11,34 @@ let imgContainer = document.querySelector('section');
 let state = new AppState();
 state.loadItems();
 
+
+
 function generateRandomProduct() {
   return Math.floor(Math.random() * state.allProducts.length);
 }
 
-function renderProductImages() {
 
+
+
+function renderProductImages() {
   while (productIndexArray.length < 6) {
     let randomProductIndex = generateRandomProduct();
     if (!productIndexArray.includes(randomProductIndex)) {
       productIndexArray.push(randomProductIndex);
     }
   }
-
   for (let i = 0; i < imgElements.length; i++) {
     let randomIndex = productIndexArray.shift()
-
     imgElements[i].src = state.allProducts[randomIndex].source
     imgElements[i].title = state.allProducts[randomIndex].name
     imgElements[i].alt = state.allProducts[randomIndex].name
     state.allProducts[randomIndex].timesShown++;
   }
 }
+
+
+
+
 
 function handleImageClick(event) {
   let imageClicked = event.target.title;
@@ -51,5 +57,10 @@ function handleImageClick(event) {
   }
 }
 
+
+
+
 renderProductImages();
+
+
 imgContainer.addEventListener('click', handleImageClick);
